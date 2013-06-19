56810845ddbb4d06c40cfbeb24b5efb7c84ec307
Andres March <andres.march@gmail.com>
2013-06-19 01:51:23 -0500

    ignore publish

--- publish.sh
+++ publish.sh
@@ -13,7 +13,7 @@ rake generate
 # Temporarily add the generated files to version control
 # (-f says "even though this file is in .gitignore")
 
-git add -f .
+git add -f public
 
 # Push them to Heroku, then repent of the commit
 
@@ -23,4 +23,4 @@ git reset --soft HEAD~1
 
 # Un-stage the generated files to finish
 
-git reset HEAD -f publish
+git reset --hard
