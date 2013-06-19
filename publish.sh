10789ead21bd287e0a002a09c3593d0d94aa693f
Andres March <andres.march@gmail.com>
2013-06-19 01:33:40 -0500

    push script

---
+++ publish.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# publish.sh
+# Build everything and deploy this app to Heroku
+
+set -e
+
+# Change to the project root directory
+
+cd "$(dirname ${BASH_SOURCE[0]})"
+
+rake generate
+
+# Temporarily add the generated files to version control
+# (-f says "even though this file is in .gitignore")
+
+git add -f .
+
+# Push them to Heroku, then repent of the commit
+
+git ci . -m 'Temporary Heroku-only deployment commit'
+git push prod master --force
+git reset --soft HEAD~1
+
+# Un-stage the generated files to finish
+
+git reset HEAD -f .
